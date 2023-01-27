d2e2c619b31f0f088b118552e12122e52f4c69df
Nic Limper <nic@xs4all.nl>
2023-01-27 20:19:40 +0100

    small fixes
    small fixes to be also compatible for compiling in Windows

--- ap_fw/cpu/8051/make.mk
+++ ap_fw/cpu/8051/make.mk
@@ -2,7 +2,7 @@ FLAGS += -Icpu/8051
 
 FLAGS += -mmcs51 --std-sdcc2x --opt-code-size --peep-file cpu/8051/peep.def --fomit-frame-pointer
 SOURCES += cpu/8051/asmUtil.c
-CC = /usr/local/bin/sdcc
+CC = sdcc
 
 TARGETS	= main.ihx main.bin
 OBJFILEEXT = rel
