dd14fd20742e950229ae7343b96426df149d9db4
Jelmer <jelmer_bruijn@hotmail.com>
2023-07-09 22:50:29 +0200

    Delete fw_dumps directory

--- fw_dumps/mkmd5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-exec("ls -1 | grep -v \".php\" | grep -v infopage | grep bin", $list);
-foreach($list as $file){
-	$data = file_get_contents($file);
-	$data = substr($data,0,10240);
-	$md5 = strtoupper(md5($data));
-	echo "$file - $md5\n";
-}
