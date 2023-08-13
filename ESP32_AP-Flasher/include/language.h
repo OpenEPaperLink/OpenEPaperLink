3fc147babe669b4b15e0044702b63bed6e7a4f3d
Moritz Wirger <lj.moritorii@web.de>
2023-08-13 09:30:51 +0200

    Remove static from language arrays reducing RAM by 5128b
    - Reduces Flash by 13060b

--- ESP32_AP-Flasher/include/language.h
+++ ESP32_AP-Flasher/include/language.h
@@ -2,32 +2,32 @@
 
 #include <Arduino.h>
 
-static int defaultLanguage = 0;
+extern int defaultLanguage;
 
-static String languageList[] = {"EN - English", "NL - Nederlands", "DE - Deutsch"};
+extern String languageList[];
 
 /*EN English language section*/
-static String languageEnDaysShort[] = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
-static String languageEnDays[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-static String languageEnMonth[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+extern String languageEnDaysShort[];
+extern String languageEnDays[];
+extern String languageEnMonth[];
 /*END English language section END*/
 
 /*NL Dutch language section*/
-static String languageNlDaysShort[] = {"ZO", "MA", "DI", "WO", "DO", "VR", "ZA"};
-static String languageNlDays[] = {"zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"};
-static String languageNlMonth[] = {"januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"};
+extern String languageNlDaysShort[];
+extern String languageNlDays[];
+extern String languageNlMonth[];
 /*END Dutch language section END*/
 
 /*DE German language section*/
-static String languageDeDaysShort[] = {"SO", "MO", "DI", "MI", "DO", "FR", "SA"};
-static String languageDeDays[] = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
-static String languageDeMonth[] = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+extern String languageDeDaysShort[];
+extern String languageDeDays[];
+extern String languageDeMonth[];
 /*END German language section END*/
 
-static String* languageDaysShort[] = {languageEnDaysShort, languageNlDaysShort, languageDeDaysShort};
-static String* languageDays[] = {languageEnDays, languageNlDays, languageDeDays};
-static String* languageMonth[] = {languageEnMonth, languageNlMonth, languageDeMonth};
+extern String* languageDaysShort[];
+extern String* languageDays[];
+extern String* languageMonth[];
 
-void updateLanguageFromConfig();
-int getDefaultLanguage();
-int getCurrentLanguage();
+extern void updateLanguageFromConfig();
+extern int getDefaultLanguage();
+extern int getCurrentLanguage();
--- ESP32_AP-Flasher/src/language.cpp
+++ ESP32_AP-Flasher/src/language.cpp
@@ -5,6 +5,32 @@
 #include "settings.h"
 #include "tag_db.h"
 
+int defaultLanguage = 0;
+
+String languageList[] = {"EN - English", "NL - Nederlands", "DE - Deutsch"};
+
+/*EN English language section*/
+String languageEnDaysShort[] = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
+String languageEnDays[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String languageEnMonth[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+/*END English language section END*/
+
+/*NL Dutch language section*/
+String languageNlDaysShort[] = {"ZO", "MA", "DI", "WO", "DO", "VR", "ZA"};
+String languageNlDays[] = {"zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"};
+String languageNlMonth[] = {"januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"};
+/*END Dutch language section END*/
+
+/*DE German language section*/
+String languageDeDaysShort[] = {"SO", "MO", "DI", "MI", "DO", "FR", "SA"};
+String languageDeDays[] = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
+String languageDeMonth[] = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+/*END German language section END*/
+
+String* languageDaysShort[] = {languageEnDaysShort, languageNlDaysShort, languageDeDaysShort};
+String* languageDays[] = {languageEnDays, languageNlDays, languageDeDays};
+String* languageMonth[] = {languageEnMonth, languageNlMonth, languageDeMonth};
+
 int currentLanguage = defaultLanguage;
 
 void updateLanguageFromConfig() {
