9f855ae1d0ef2fcc1fc1f5a00f524337ebe1594f
Moritz Wirger <lj.moritorii@web.de>
2023-09-14 21:31:56 +0200

    Use String& for wsLog, wsErr and wsSerial

--- ESP32_AP-Flasher/include/web.h
+++ ESP32_AP-Flasher/include/web.h
@@ -6,12 +6,12 @@
 void init_web();
 void doImageUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final);
 void doJsonUpload(AsyncWebServerRequest *request);
-void wsLog(String text);
-void wsErr(String text);
+void wsLog(const String &text);
+void wsErr(const String &text);
 void wsSendTaginfo(const uint8_t *mac, uint8_t syncMode);
 void wsSendSysteminfo();
 void wsSendAPitem(struct APlist *apitem);
-void wsSerial(String text);
+void wsSerial(const String &text);
 uint8_t wsClientCount();
 
 extern AsyncWebSocket ws;
